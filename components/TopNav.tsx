4d6ff3a36f2cacce1debb602fb91e170207cdeab
Gerazel Castro <gerazelcastro@gmail.com>
2025-06-01 10:57:13 -0600

    TopNav-Instagram

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,47 +1,53 @@
 import React from 'react';
-import { View, Image, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { LinearGradient } from 'expo-linear-gradient';
-
-// //Please install 
-// npx expo install expo-linear-gradient
-// npx expo install @expo/vector-icons
-// npx expo install react-native-svg
-
+import Octicons from '@expo/vector-icons/Octicons';
 export default function TopNav() {
   const handlePress = () => {
     Alert.alert("Assignment 1 Completed");
   };
-//searched google for "expo linear gradient"
+
   return (
-    <LinearGradient 
-      colors={['#528D8F', '#528D8F', '#C1D8D5', '#F4F7F4']}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 0, y: 1 }}
-      style={styles.container}
-    >
-      <Ionicons name="mail" size={28} color="#FFFFFF" />
-      <Image
-        source={require('../assets/suntea-logo.png')}
-        style={styles.logo}
-        resizeMode="contain"
-      />     
-      <Ionicons name="menu" size={32} color="#FFFFFF" />
-    </LinearGradient>
+    <View style={styles.container}>
+      {/* Back Button */}
+      <TouchableOpacity onPress={handlePress}>
+        <Ionicons name="chevron-back" size={24} color="black" />
+      </TouchableOpacity>
+
+      {/* Center Text */}
+      <View style={styles.centerText}>
+        <Text style={styles.title}>Group Profile</Text>
+        <Text style={styles.subtitle}>ootd_everyday</Text>
+      </View>
+
+      {/* Add Button */}
+      <TouchableOpacity onPress={handlePress}>
+      <Octicons name="diff-added" size={24} color="#3D403D" />
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
     paddingTop: 50,
-    paddingHorizontal: 20,
     paddingBottom: 10,
+    paddingHorizontal: 15,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-  logo: {
-    width: 100,
-    height: 40,
+  centerText: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  subtitle: {
+    fontSize: 12,
+    color: '#909E8B',
   },
 });
